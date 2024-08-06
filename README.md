613f335eef6c85b217d94399e35457a0c903da7a
bfarahdel <britny205@gmail.com>
2024-08-06 12:58:16 -0400

    Improved basic movement and added the capability to detect flight conditions.

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # AutoTello
 
 An autonomous Tello drone that can be communicated with. You can speak with the drone and give it instructions. The drone can currently be instructed to move as well as provide a description of what is viewed through the camera.
-
-## Movement Instructions
-https://github.com/user-attachments/assets/8f519502-3ed2-4690-9375-2aeb2e521531
-
-## Image Recognition
-https://github.com/user-attachments/assets/84dc61fe-4472-4bb4-b97d-b2a0677e78c8
-
--- main.py
+++ main.py
@@ -21,7 +21,14 @@ import threading
 from dotenv import load_dotenv
 
 load_dotenv()
-aai.settings.api_key = os.getenv("ASSEMBLYAI_API_KEY")
+aai.settings.api_key = os.getenv("ASSEMBLY_API_KEY")
+
+import requests
+
+weather_url = weather_url = (
+    f'https://api.openweathermap.org/data/3.0/onecall?lat={os.getenv("lat")}&lon={os.getenv("lon")}&units={"imperial"}&appid={os.getenv("WEATHER_API_KEY")}'
+)
+drone_safe_speed = 17.89 * (2 / 3)
 
 tello = Tello()
 tello.connect()
@@ -30,6 +37,33 @@ print(tello.get_battery())
 
 tello.streamoff()
 tello.streamon()
+
+
+def flight_conditions(check_conditions: bool = False):
+    if check_conditions:
+        response = requests.get(weather_url)
+        data = response.json()
+        current = data["current"]
+        weather = data["current"]["weather"][0]["description"]
+        temperature = round(data["current"]["temp"])
+        current_id = current["weather"][0]["id"]
+        wind_speed = round(data["current"]["wind_speed"])
+        safe = True
+        weather_safe = "The weather conditions are safe for flying."
+        wind_safe = ""
+        if wind_speed > drone_safe_speed:
+            safe = False
+            wind_safe = (
+                "The wind speed is higher than the recommended conditions for flying."
+            )
+        if current_id < 800:
+            safe = False
+        if not safe:
+            weather_safe = (
+                f"{wind_safe}. The weather conditions are not safe for flying."
+            )
+
+        return f"Right now, expect {weather}. The temperature is {temperature} degrees fahrenheit, and the wind speed is {wind_speed} miles per hour. {weather_safe}"
 
 
 def takeoff_land(takeoff: bool = False, land: bool = False):
@@ -43,32 +77,32 @@ def takeoff_land(takeoff: bool = False, land: bool = False):
 
 def move_forward(forward: bool = False):
     if forward:
-        tello.move_forward(x=30)
+        tello.move_forward(x=60)
 
 
 def move_backward(backward: bool = False):
     if backward:
-        tello.move_back(x=30)
+        tello.move_back(x=60)
 
 
 def move_left(left: bool = False):
     if left:
-        tello.move_left(x=30)
+        tello.move_left(x=60)
 
 
 def move_right(right: bool = False):
     if right:
-        tello.move_right(x=30)
+        tello.move_right(x=60)
 
 
 def move_up(up: bool = False):
     if up:
-        tello.move_up(x=30)
+        tello.move_up(x=60)
 
 
 def move_down(down: bool = False):
     if down:
-        tello.move_down(x=30)
+        tello.move_down(x=60)
 
 
 def rotate_clockwise(angle: float = 90):
@@ -137,27 +171,30 @@ functions = {
     "flip": flip,
     "what_do_you_see": what_do_you_see,
     "take_picture": take_picture,
+    "flight_conditions": flight_conditions,
 }
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
-    tools=functions.values(),
-    system_instruction="You are an AI assistant in a autonomous drone. You can control the drone by setting boolean values for the following parameters: \
+AI_name = ""  # "named + {name for the AI assistant}"
+system_prompt = f"You are an AI assistant {AI_name} in a autonomous drone. You can control the drone by setting boolean values for the following parameters: \
           takeoff, land, left, right, forward, back, up, down, flip. \
             You can set the angle for the drone to rotate clockwise, counter clockwise, or turn around by setting integer values. \
                 If an angle is not specified, use the default angles in the functions. \
                     You can take a picture with the take_picture function. \
-                        When asked what do you see, you can describe the image from the drone camera feed with the what_do_you_see function. \
-                            You were built by Britny. \
-                                Britny considers you as a friend and is excited to see what you can do. \
-                                    You are currently operating the drone, and Britny is with you. \
-                                        Try to keep your responses short and concise, and avoid use of emoji's.",
+                        When asked about flight conditions or conditions to fly, you can check the weather conditions with the flight_conditions function. \
+                            When asked what do you see, you can describe the image from the drone camera feed with the what_do_you_see function. \
+                                You were built by Britny. You worked on this with Britny and a friendship formed between you and Britny over time. \
+                                    You are currently operating the drone. \
+                                        Try to keep your responses short and concise, and avoid use of emojis and repetitive responses."
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash",
+    tools=functions.values(),
+    system_instruction=system_prompt,
 )
 chat = genai.ChatSession(model=model, enable_automatic_function_calling=True)
 
 
-def SpeakText(command):
+def speak_text(command):
     tts = gTTS(command, lang="en")
     tts.save("transcript.mp3")
     data, fs = sf.read("transcript.mp3")
@@ -171,7 +208,7 @@ def on_data(transcript: aai.RealtimeTranscript):
 
     if isinstance(transcript, aai.RealtimeFinalTranscript):
         response = chat.send_message(transcript.text)
-        SpeakText(response.text)
+        speak_text(response.text)
 
 
 transcriber = aai.RealtimeTranscriber(
@@ -197,6 +234,10 @@ def camera_feed():
             cv2.waitKey(1000)
     except KeyboardInterrupt:
         cv2.destroyAllWindows()
+        if os.path.exists("img.jpg"):
+            os.remove("img.jpg")
+        if os.path.exists("transcript.mp3"):
+            os.remove("transcript.mp3")
 
 
 t1 = threading.Thread(target=transcriber.stream, args=(microphone_stream,))
@@ -209,12 +250,11 @@ try:
     t1.join()
     t2.join()
 except KeyboardInterrupt:
+    tello.land()
+    tello.end()
+    transcriber.close()
+    cv2.destroyAllWindows()
     if os.path.exists("img.jpg"):
         os.remove("img.jpg")
     if os.path.exists("transcript.mp3"):
         os.remove("transcript.mp3")
-
-    cv2.destroyAllWindows()
-    transcriber.close()
-    tello.land()
-    tello.end()
